aab17eb3472570b4c0f21a62caa8c76dbe466563
sjgraffjr <sjgraffjr@gmail.com>
2017-12-07 20:52:29 -0600

    changed error in http

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './Header/header.js';
 import Footer from './Footer/Footer.js';
 import Map from './Map/Map.js'
-const BACKEND_URL = 'hhttps://brewery-map-backend.herokuapp.com'
+const BACKEND_URL = 'https://brewery-map-backend.herokuapp.com'
 
 class App extends Component {
   
